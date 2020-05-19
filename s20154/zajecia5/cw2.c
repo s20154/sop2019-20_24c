267c965c5b276fde791d4bf14a6ebec5bc66b6ce
Kijanczuk, Damian <s20154@pjwstk.edu.pl>
2020-05-19 15:35:59 +0200

    Add exercises from 5th meet

---
+++ s20154/zajecia5/cw2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(){
+	pid_t pid = fork();
+	printf("Pid %d", getpid());
+	printf("PPid %d", getppid());
+	if( pid > 0) {
+		printf("Rodzic czeka na dziecko\n");
+		fflush(stdout);
+		wait(NULL);
+		printf("Czekamy");
+		fflush(stdout);
+	} else {
+		printf("Dziecko operuje\n");
+		fflush(stdout);
+		sleep(3);
+		printf("Czekamy");
+		fflush(stdout);
+	}
+
+	return 0;
+}
+
+
